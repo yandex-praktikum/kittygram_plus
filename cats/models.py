4b63c1fb8dc4265d341f53e077fd7474341b5c47
Dmitry Medvedev <mds@mail.ru>
2023-03-10 12:52:01 +0500

    add owner model

--- cats/models.py
+++ cats/models.py
@@ -1,11 +1,26 @@
 from django.db import models
 
 
+class Owner(models.Model):
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+    
+
 class Cat(models.Model):
     name = models.CharField(max_length=16)
     color = models.CharField(max_length=16)
     birth_year = models.IntegerField()
+    owner = models.ForeignKey(
+        Owner,
+        related_name='cats',
+        on_delete=models.CASCADE,
+    )
 
 
     def __str__(self):
         return self.name
+    
